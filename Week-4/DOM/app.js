25cbff6b7589bb91478734335febbc698d116e56
Geethapranay1 <geethapranay.official@gmail.com>
2024-03-12 07:15:08 +0530

    Week-4 codes of first part completed

---
+++ Week-4/DOM/app.js
@@ -0,0 +1,17 @@
+
+
+
+
+function sumCalculate() {
+  const num1 = parseInt(document.getElementById("firstNumber").value);
+  const num2 = parseInt(document.getElementById("secondNumber").value);
+  document.getElementById("finalAnswer").innerHTML = "The sum is " + (num1 + num2);
+}
+
+// Add event listener to the button
+// const button = document.getElementById("btn");
+// button.addEventListener("click", sumCalculate); // Call the function on click
+
+
+
+ 
