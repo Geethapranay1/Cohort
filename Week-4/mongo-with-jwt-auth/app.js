fa16775921d5fe73118b97b279dd80e8c346cce7
Geethapranay1 <geethapranay.official@gmail.com>
2024-03-18 17:20:13 +0530

    Week-4 has been modified

--- Week-4/mongo-with-jwt-auth/app.js
+++ Week-4/mongo-with-jwt-auth/app.js
@@ -3,18 +3,18 @@ const bodyParser = require("body-parser");
 const app = express();
 const adminRoute = require("./routes/admin");
 const userRoute = require("./routes/user");
-
+require("dotenv").config();
 const JWT_SECRET = "pranay";
+
+const port = process.env.PORT || 3001;
 
 
 app.use(bodyParser.json());
 app.use("/admin", adminRoute);
 app.use("/user", userRoute);
 
-const PORT = 3000;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 }) 
 
 
